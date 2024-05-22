b8f5d4612d6612f647ab0394f2c5824eeda2c189
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-22 12:11:26 +0800

    Update control.py

--- control.py
+++ control.py
@@ -10,7 +10,7 @@ app=Flask(__name__)
 
 
 # 只接受get方法访问
-@app.route("/command",methods=["GET"])
+@app.route("/command",methods=["POST"])
 def check():
     # 默认返回内容
     return_dict= {'return_code': '200', 'return_info': '处理成功', 'result': False}
@@ -20,7 +20,9 @@ def check():
         return_dict['return_info'] = '请求参数为空'
         return json.dumps(return_dict, ensure_ascii=False)
     # 获取传入的params参数
-    get_data=request.args.to_dict()
+    # get_data=request.args.to_dict()
+    get_data=request.json
+    print(get_data)
     command_list = json.loads(get_data.get('command'))
     try:
         export_dict = json.loads(get_data.get('export_dict'))
@@ -33,13 +35,14 @@ def check():
     work_path = ''
     for command in command_list:
         one_command = ' '.join(command)   
+        print(one_command)
         # 将命令写入 a.sh 文件中
         with open('opera.sh', 'a') as f:
             f.write(f'{one_command}\n')   
     out = subprocess.run(['bash', 'opera.sh'], capture_output=True, text=True).stdout
     return_dict['result'] = True
     return_dict['out'] = out
-    subprocess.run(['rm', '-f', 'opera.sh'])
+    # subprocess.run(['rm', '-f', 'opera.sh'])
     base_info = json.loads(get_data.get('base_info'))
     if base_info['opera'] in ['启动服务', '重启服务']:
         remote_addr = base_info['remote_addr']
