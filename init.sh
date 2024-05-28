2fde99edcb0bf6a56b7bee117a7a9df0710446ad
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-28 13:32:12 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,9 @@ apt install -yq python3-pip
 pip3 install flask
 pip3 install apscheduler
 pip3 install requests
+# 打开防火墙
+sudo ufw enable
+sudo ufw allow 80/tcp
+sudo ufw reload
 cd ~
 nohup python3 system_init/control.py &
