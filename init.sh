c1783470d9e04d66ee4d6eb664ff78a31f7e87c3
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-29 18:39:29 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+echo "\$nrconf{kernelhints} = 0;" >> /etc/needrestart/needrestart.conf
+echo "\$nrconf{restart} = 'l';" >> /etc/needrestart/needrestart.conf
 sudo apt-get update
 apt install -yq python3-pip 
 pip3 install flask
